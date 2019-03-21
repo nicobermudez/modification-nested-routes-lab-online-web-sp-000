94a7406c913920b8aa8bce5326f8a97bb57ed503
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-21 16:49:57 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,7 @@ class SongsController < ApplicationController
   end
 
   def new
-    @artist = 
+    @artist = Artist.find_by(id: params[:artist_id])
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
       redirect_to artists_path, alert: "Artist not found"
     else
